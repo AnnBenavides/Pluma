e6e83a54010a7acf5027f37c711761cccaa754dc
Andrea Benavides <andrea.benavidesj@gmail.com>
2016-11-28 18:03:48 -0300

    agregué la clase Cuadro con algunas observaciones

---
+++ src/core/Cuadro.java
@@ -0,0 +1,50 @@
+package core;
+
+public class Cuadro {
+	private char c; //COLOR puede ser objeto de Color (?)
+	private int i; //posx
+	private int j; //posi
+	private boolean esBorde;
+	
+	/** TODO 
+	 * [x] gets de coordenadas y color
+	 * [x] cambiar color
+	 * [x] tipo borde (Se podria hacer una subclase en vez de un booleano(?))
+	 * **/
+	
+	Cuadro(int x,int y){
+		c = 'B';// parte en blanco
+		i = x;
+		j = y;
+		esBorde();
+	}
+	
+	public char getcol(){
+		return c;
+	}
+	
+	public int getposx(){
+		return i;
+	}
+	
+	public int getposy(){
+		return j;
+	}
+	
+	public void col(char color){
+		c=color;
+	}
+	
+	public boolean borde(){
+		return esBorde;
+	}
+
+	private void esBorde(){
+		if(i==1 || i==50 || j==0 || j==50){
+			esBorde = true;
+		} else {
+			esBorde = false;
+		}
+		
+	}
+}
