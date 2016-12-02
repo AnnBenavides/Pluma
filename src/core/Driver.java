4687859a8af753cda8e7d41637446acb99910da7
Andrea Benavides <andrea.benavidesj@gmail.com>
2016-12-01 21:13:29 -0300

    Main class, Flex and Cup bases

---
+++ src/core/Driver.java
@@ -0,0 +1,17 @@
+package core;
+
+/** Clase principal donde se corre todo
+ * TODO
+ * [] recibe como argumento el nombre del archivo de texto
+ * [] se inicializa la pluma y el tablero
+ * [] stack de operaciones (?) 
+ * [] hacer correr el parser (flex y cup)
+ * [] mostrar
+ * **/
+
+public class Driver {
+
+	public static void main (String [] args){
+		
+	}
+}
